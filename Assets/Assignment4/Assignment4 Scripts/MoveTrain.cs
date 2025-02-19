dbbcf1870a1e51ed1ac361e0970170dcf634f99c
Dorian <dorianmuhametaj0920@gmail.com>
2025-02-18 21:03:36 -0500

    Made a Go function so that the slider would determine the position of the train on the rails.

--- Assets/Assignment4/Assignment4 Scripts/MoveTrain.cs
+++ Assets/Assignment4/Assignment4 Scripts/MoveTrain.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MoveTrain : MonoBehaviour
 {
+    public float pos; 
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,23 @@ public class MoveTrain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Creates a vector2 to store a new x value that can later be used to update our objects position 
+        Vector2 mouseX;
+        //Stores the objects position before updating with a new x value
+        mouseX = transform.position;
+        mouseX.x = pos;
+       
+        //Returns the same y value as was stored but now with an updated x so our object can match the position of the slider. 
+        transform.position = mouseX; 
+
+        
+    }
+
+    //Creates a public void go for our slider to reference 
+    public void Go(float s)
+    {
+        //Updates the position to the value of the slider 
+        pos = s;
         
     }
 }
